2b2df30a5f22678f0bae60251b873fcffed1751f
Scott Hanson <scooter_seh@yahoo.com>
2024-07-05 13:49:20 -0400

    rename

--- src/config.h
+++ src/config.h
@@ -1,10 +1,10 @@
 #ifndef INCLUDE_GUARD
 #define INCLUDE_GUARD
 
-#define PROJECT_NAME "CapeEEPROMViewer"
-#define PROJECT_VER  "0.0.2"
+#define PROJECT_NAME "ESPHomeManager"
+#define PROJECT_VER  "0.0.1"
 #define PROJECT_VER_MAJOR "0"
 #define PROJECT_VER_MINOR "0"
-#define PTOJECT_VER_PATCH "2"
+#define PTOJECT_VER_PATCH "1"
 
 #endif // INCLUDE_GUARD
